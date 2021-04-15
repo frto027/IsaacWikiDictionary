34e05269b6dae108980799d348b5360ac0d0c091
frto027 <602706150@qq.com>
2021-04-15 18:20:45 +0800

    change display logic for auth texts

--- patch.lua
+++ patch.lua
@@ -188,6 +188,29 @@ end
 function WikiDic:RenderCallback()
 	WikiDic:InitFonts()
 
+	-- draw auth infos here
+	if WikiDic.authRemains > 0 then
+		if WikiDic.targetEntity ~= nil then
+			WikiDic.authRemains = 0
+		end
+
+		WikiDic.authRemains = WikiDic.authRemains - 1
+		local screen_center = Isaac.WorldToRenderPosition(Vector(320,240))
+
+		local y_pos = screen_center.Y - #WikiDic.authTexts * WikiDic.font:GetLineHeight() / 2
+		local alpha = 0.6 --math.cos(WikiDic.authRemains * 0.1) * 0.1 + 0.5
+		if WikiDic.authRemains < 120 then
+			alpha = alpha * WikiDic.authRemains / 120
+		end
+
+		for _,text in pairs(WikiDic.authTexts) do
+			WikiDic.font:DrawStringScaledUTF8(text,WikiDic.renderPos.X,y_pos,WikiDic.fontScale,WikiDic.fontScale,KColor(1,1,1,alpha,0,0,0),0,true)
+			y_pos = y_pos + WikiDic.font:GetLineHeight()
+		end
+		return
+	end
+
+
 	if WikiDic.drawMouse then
 		if WikiDic.cursur_sprite == nil then
 			WikiDic.cursur_sprite = Sprite()
@@ -243,30 +266,5 @@ function WikiDic:RenderCallback()
 	end
 end
 
-function WikiDic:RenderAuthorityCallback()
-	WikiDic:InitFonts()
-	if WikiDic.authRemains > 0 then
-		WikiDic.authRemains = WikiDic.authRemains - 1
-	end
-	if WikiDic.authRemains <= 0 or WikiDic.targetEntity ~= nil then
-		WikiDic:RemoveCallback(ModCallbacks.MC_POST_RENDER,WikiDic.RenderAuthorityCallback)
-	end
-	
-	local screen_center = Isaac.WorldToRenderPosition(Vector(320,240))
-
-	local y_pos = screen_center.Y - #WikiDic.authTexts * WikiDic.font:GetLineHeight() / 2
-	local alpha = 0.6 --math.cos(WikiDic.authRemains * 0.1) * 0.1 + 0.5
-	if WikiDic.authRemains < 120 then
-		alpha = alpha * WikiDic.authRemains / 120
-	end
-
-	for _,text in pairs(WikiDic.authTexts) do
-		WikiDic.font:DrawStringScaledUTF8(text,WikiDic.renderPos.X,y_pos,WikiDic.fontScale,WikiDic.fontScale,KColor(1,1,1,alpha,0,0,0),0,true)
-		y_pos = y_pos + WikiDic.font:GetLineHeight()
-	end
-
-end
-
 WikiDic:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE,WikiDic.Update)
 WikiDic:AddCallback(ModCallbacks.MC_POST_RENDER,WikiDic.RenderCallback)
-WikiDic:AddCallback(ModCallbacks.MC_POST_RENDER,WikiDic.RenderAuthorityCallback)
